6789eaaa48d08b361b3196e0ab439d02ba46679d
uchan-nos <uchan@uchan.net>
2025-07-21 21:27:04 +0900

    テスト修正

--- tests/test_review.py
+++ tests/test_review.py
@@ -19,52 +19,6 @@ def read_config_for_test():
     args.log_level = None
     return kidivis.review.read_config(args)
 
-class TestGitOperations(unittest.TestCase):
-    def setUp(self):
-        self.tmpdir = Path(tempfile.mkdtemp(prefix='kidivis'))
-        print(f'temporary directory: {self.tmpdir}')
-        (self.tmpdir / 'extract').mkdir()
-
-        kicad_files = ['sample.kicad_' + kind for kind in ['pro', 'sch', 'pcb']]
-
-        self.git_repo = git.Repo.init(self.tmpdir / 'repo')
-        repo_root = self.git_repo.working_tree_dir
-
-        for f in kicad_files:
-            shutil.copy(kicad_files_dir / 'sample1' / f, repo_root)
-        self.git_repo.index.add(kicad_files)
-        self.initial_commit = self.git_repo.index.commit('initial commit')
-
-        for f in kicad_files:
-            shutil.copy(kicad_files_dir / 'sample2' / f, repo_root)
-        self.git_repo.index.add(kicad_files)
-        self.second_commit = self.git_repo.index.commit('second commit')
-
-        for f in kicad_files:
-            shutil.copy(kicad_files_dir / 'sample3' / f, repo_root)
-        # コミットしない。変更がワーキングツリーだけにある状態とする。
-
-    def tearDown(self):
-        shutil.rmtree(self.tmpdir)
-
-    def test_extract_file_git_from_initial_commit(self):
-        # initial commit のファイル抽出
-        output_path = self.tmpdir / 'extract/initial_commit.kicad_pcb'
-        kidivis.review.extract_file_git(self.git_repo,
-                                        str(self.initial_commit),
-                                        self.tmpdir / 'repo/sample.kicad_pcb',
-                                        output_path)
-        self.assertFalse(diff_files(output_path, kicad_files_dir / 'sample1/sample.kicad_pcb'))
-
-    def test_extract_file_git_from_working_tree(self):
-        # ワーキングツリーのファイル抽出
-        output_path = self.tmpdir / 'WORK' / 'extract/working_tree.kicad_pcb'
-        kidivis.review.extract_file_git(self.git_repo,
-                                        None,
-                                        self.tmpdir / 'repo/sample.kicad_pcb',
-                                        output_path)
-        self.assertFalse(diff_files(output_path, kicad_files_dir / 'sample3/sample.kicad_pcb'))
-
 class TestSVGOperations(unittest.TestCase):
     def setUp(self):
         self.tmpdir = Path(tempfile.mkdtemp(prefix='kidivis'))
